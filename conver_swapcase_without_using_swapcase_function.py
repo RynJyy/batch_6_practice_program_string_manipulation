bc6f52de09fdb4972b3578c9696661a2d232fe44
RynJyy <ryancanlas299@gmail.com>
2025-03-28 22:25:51 +0800

    final prog6.8

--- conver_swapcase_without_using_swapcase_function.py
+++ conver_swapcase_without_using_swapcase_function.py
@@ -1,3 +1,13 @@
 #input characters
+statement = input("Enter statement: ")
 #convert to swapcase without using swapcase function
+swap_statement = ""
+for i in statement:
+    if i.islower():
+        swap_statement += i.upper()
+    elif i.isupper():
+        swap_statement += i.lower()
+    else:
+        swap_statement += i
 #print output
+print(swap_statement)
