37e74d9d36147d93243771267ad0560dcebbad4b
RynJyy <ryancanlas299@gmail.com>
2025-03-28 23:14:05 +0800

    final prog6.10

--- capitalized_first_letter_of_each_word_without_using_title_function.py
+++ capitalized_first_letter_of_each_word_without_using_title_function.py
@@ -1,3 +1,12 @@
 #input statement
+statement = input("Enter statement: ")
 #capitalized first letter of every words without using title function
+capitalized_words = []
+
+words = statement.split()
+for word in words:
+    capitalized_word = word[0].upper() + word[1:].lower()
+    capitalized_words.append(capitalized_word)
 #print statement
+result = " ".join(capitalized_words)
+print(result)
