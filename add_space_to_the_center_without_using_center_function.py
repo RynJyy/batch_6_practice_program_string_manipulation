792a4217c0229840794ef3540e047b8a83c85eb5
RynJyy <ryancanlas299@gmail.com>
2025-03-28 22:05:32 +0800

    final prog6.7

--- add_space_to_the_center_without_using_center_function.py
+++ add_space_to_the_center_without_using_center_function.py
@@ -1,4 +1,9 @@
 #input character
+characters = input("Enter statement: ")
 #input how many spaces should be added
+spaces = int(input("Enter number of spaces: "))
 #add space without using the center function
+middle_characters = len(characters) // 2
+added_spaces_characters = characters[:middle_characters] + " " * spaces + characters[middle_characters:]
 #print output
+print (added_spaces_characters)
