518659d4c91b620d5286248951e4467ec726459c
RynJyy <ryancanlas299@gmail.com>
2025-03-28 19:56:30 +0800

    final prog6.3

--- convert_into_lower_case_dont_use_lower_function.py
+++ convert_into_lower_case_dont_use_lower_function.py
@@ -1,3 +1,7 @@
 #input characters
+statement = input("Enter characters: ")
 #convert into lower case without using lower()
+statement = statement.upper()
+statement = statement.swapcase()
 #print characters
+print (statement)
