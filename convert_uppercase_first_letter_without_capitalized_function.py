7b6292cf76011d3a873807567ecb362de8278daf
RynJyy <ryancanlas299@gmail.com>
2025-03-28 22:57:23 +0800

    final prog6.9

--- convert_uppercase_first_letter_without_capitalized_function.py
+++ convert_uppercase_first_letter_without_capitalized_function.py
@@ -1,3 +1,6 @@
 #input statement
+statement = input("Enter statement: ")
 #capitalized the first letter without using capitalized
+statement = statement[0].upper() + statement[1:].lower()
 #print statement
+print (statement)
