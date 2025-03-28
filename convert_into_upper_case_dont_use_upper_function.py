e82ebbd83852d8150d1dddd66b77915559afc720
RynJyy <ryancanlas299@gmail.com>
2025-03-28 21:03:46 +0800

    final prog6.4

--- convert_into_upper_case_dont_use_upper_function.py
+++ convert_into_upper_case_dont_use_upper_function.py
@@ -1,3 +1,7 @@
 #input characters
+statement = input("Enter statement: ")
 #convert into upper case without using upper()
+statement = statement.lower()
+statement = statement.upper()
 #print characters
+print (statement)
