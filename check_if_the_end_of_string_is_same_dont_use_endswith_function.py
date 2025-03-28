92bd8aac2396aa45cd957dad6459d5e08b39b723
RynJyy <ryancanlas299@gmail.com>
2025-03-28 21:50:14 +0800

    final prog6.5

--- check_if_the_end_of_string_is_same_dont_use_endswith_function.py
+++ check_if_the_end_of_string_is_same_dont_use_endswith_function.py
@@ -1,3 +1,9 @@
 #input character
+character = input("Enter statement: ")
 #input suffix
+suffix = input("suffix: ")
 #check if true or false (dont use endswith())
+if character[len(character) - len(suffix):] == suffix:
+    print ("true")
+else:
+    print ("false")
