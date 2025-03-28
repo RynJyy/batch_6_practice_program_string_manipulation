ba7157af561397ca333e48d28a8ab6694cacc6ce
RynJyy <ryancanlas299@gmail.com>
2025-03-28 19:48:41 +0800

    final prog6.2

--- remove_characters_at_beginning_of_string_dont_use_removeprefix.py
+++ remove_characters_at_beginning_of_string_dont_use_removeprefix.py
@@ -1,3 +1,8 @@
 #input statement
+statement = input("Enter string: ")
+prefix = input("Enter prefix: ")
 #remove characters at beginning without using removeprefix()
+if statement.startswith(prefix):
+    statement = statement[len(prefix):]
 #print statement
+print (statement)
