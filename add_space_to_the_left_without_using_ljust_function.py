c68f81633edae1b440f8758f4a4e307674de96b1
RynJyy <ryancanlas299@gmail.com>
2025-03-28 21:33:14 +0800

    final prog6.6

--- add_space_to_the_left_without_using_ljust_function.py
+++ add_space_to_the_left_without_using_ljust_function.py
@@ -1,4 +1,11 @@
 #input statement
+statement = input("Enter statement: ")
 #input width
+width = int(input("Enter total width: "))
 #add space to the left without using ljust
+space = width
+while len(statement) < width:
+    statement = statement + " "
+    space += 1
 #print output
+print (f'"{statement}"')
