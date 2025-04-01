64650e46fe005d0c9befd8e90a26469fb3058511
RynJyy <ryancanlas299@gmail.com>
2025-04-01 19:21:04 +0800

    final prog7.9

--- find_index_of_character_without_using_index_function.py
+++ find_index_of_character_without_using_index_function.py
@@ -1,4 +1,14 @@
 #input statement
+statement = input("Enter statement: ")
 #input word to find
+word = input("Enter word to find: ")
 #find word in statement without using index()
+position = 0
+for i in range(len(statement)):
+    if statement[i:i+len(word)] == word:
+        position = i
 #print output
+if position != -1:
+    print(position)
+else:
+    print("Substring not found")
