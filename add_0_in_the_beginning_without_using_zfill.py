db382bab2c9fa1b92fdca592c3f4f2f5f6d02a08
RynJyy <ryancanlas299@gmail.com>
2025-04-01 18:43:51 +0800

    final prog7.7

--- add_0_in_the_beginning_without_using_zfill.py
+++ add_0_in_the_beginning_without_using_zfill.py
@@ -1,4 +1,11 @@
 #input character
+number = str(input("Enter number: "))
 #input width
+width = int(input("Enter width: "))
 #add 0 to the beginning without using zfill()
+zeroes = width
+while len(number) < width:
+    number = "0" + number
+    zeroes += 1
 #print output
+print (number)
