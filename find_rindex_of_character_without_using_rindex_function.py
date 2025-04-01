9f5e04eaa8993eef1325c896cd5443809cc75a8e
RynJyy <ryancanlas299@gmail.com>
2025-04-01 19:32:00 +0800

    final prog7.10

--- find_rindex_of_character_without_using_rindex_function.py
+++ find_rindex_of_character_without_using_rindex_function.py
@@ -1,4 +1,14 @@
 #input statement
+statement = input("Enter statement: ")
 #input word to find
+word = input("Enter word to find: ")
 #find the first location of the word in statement without using rindex()
+position = -1
+for i in range(len(statement)):  
+    if statement[i:i + len(word)] == word:
+        position = i
 #print output
+if position != -1:
+    print(position)
+else:
+    print("Substring not found")
