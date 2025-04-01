b5db9de7316266a8fa58ac4b7abb99ad0a8513a1
RynJyy <ryancanlas299@gmail.com>
2025-04-01 19:31:30 +0800

    update prog7.9

--- find_index_of_character_without_using_index_function.py
+++ find_index_of_character_without_using_index_function.py
@@ -7,8 +7,11 @@ position = 0
 for i in range(len(statement)):
     if statement[i:i+len(word)] == word:
         position = i
+        break
 #print output
 if position != -1:
     print(position)
 else:
     print("Substring not found")
+
+    
