cd320868cea59b7f98e73057cf417fdfd25b6d5f
RynJyy <ryancanlas299@gmail.com>
2025-04-01 18:32:43 +0800

    final prog7.6

--- add_space_to_the_beginning_without_using_rjust.py
+++ add_space_to_the_beginning_without_using_rjust.py
@@ -1,4 +1,11 @@
 #input character
+statement = input("Enter statement: ")
 #input width
+width = int(input("Enter width: "))
 #add space to the beginning without using rjust ()
+space = width
+while len(statement) < width:
+    statement = " " + statement
+    space += 1
 #print output
+print (f'"{statement}"')
