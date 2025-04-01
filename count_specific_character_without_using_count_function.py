2b3c17715c820411c2d9798e55647724811b3ce4
RynJyy <ryancanlas299@gmail.com>
2025-04-01 19:12:51 +0800

    final prog7.8

--- count_specific_character_without_using_count_function.py
+++ count_specific_character_without_using_count_function.py
@@ -1,4 +1,11 @@
 #input statement
+statement = input("Enter statement: ")
 #input what character to count
+word = input("Enter word to count: ")
 #count specific character without using count()
+count = 0
+for i in range(len(statement) - len(word) + 1):
+    if statement[i:i + len(word)] == word:
+        count += 1
 #print output
+print (count)
