4a172a6036c79f5f98e85779d061404a1d2fd89d
RynJyy <ryancanlas299@gmail.com>
2025-04-01 19:33:09 +0800

    update prog7.10

--- find_rindex_of_character_without_using_rindex_function.py
+++ find_rindex_of_character_without_using_rindex_function.py
@@ -2,7 +2,7 @@
 statement = input("Enter statement: ")
 #input word to find
 word = input("Enter word to find: ")
-#find the first location of the word in statement without using rindex()
+#find the first location of the word in statement starting from last character without using rindex()
 position = -1
 for i in range(len(statement)):  
     if statement[i:i + len(word)] == word:
